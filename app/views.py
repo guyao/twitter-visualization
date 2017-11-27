03b073a7291e80965e34bdd6b44e62d3f87e8e1c
Yao Gu <guyao@guyao.me>
2017-11-26 22:26:19 -0800

    update & new data

--- app/views.py
+++ app/views.py
@@ -9,7 +9,7 @@ import mpld3
 from app import app
 from app.data import X, labels, label_vector
 
-N = 500
+N = 30000
 N_neighbor = 400
 
 
@@ -26,6 +26,7 @@ def api():
     elif request.method == "POST":
         q = request.form.get('q', '')
     q = q if q.startswith("#") else "#" + q
+    q = q.strip()
     if q not in label_vector:
         return "Hashtag does not exist"
 
@@ -36,14 +37,14 @@ def api():
     label_sample = [labels[i] for i in idx]
 
     inx = label_vector[q]
-    idx = calc_n_neighbor(inx[np.newaxis, :], X_sample)
-    idx = idx[-N_neighbor:]
+    idx = calc_n_neighbor(inx[np.newaxis, :], X_sample, N_neighbor)
 
     lbs = [label_sample[i] for i in idx]
 
     fig = plot_interactive_scatter(
         X_sample[idx, :], lbs, inx[np.newaxis, :], q)
     return mpld3.fig_to_html(fig)
+
 
 @app.route("/hashtags", methods=['GET'])
 def hashtags():
@@ -59,33 +60,30 @@ def neighbor(q=None):
             q = request.args.get('q', '')
         elif request.method == "POST":
             q = request.form.get('q', '')
+    q = q.strip()
     if not q:
         return "invalid input"
-
     q = q if q.startswith("#") else "#" + q
+
     if q not in label_vector:
         return "Hashtag does not exist"
 
     inx = label_vector[q]
-    idx = calc_n_neighbor(inx[np.newaxis, :], X)
-    idx = idx[-N_neighbor:]
+    idx = calc_n_neighbor(inx[np.newaxis, :], X, N_neighbor)
     lbs = [labels[i] for i in idx]
 
     return render_template("neighbor.html", hashtags=lbs)
 
 
-
-
-def calc_n_neighbor(inX, X):
-    # distances = sklearn.metrics.pairwise.euclidean_distances(X, inX)
-    distances = sklearn.metrics.pairwise.cosine_distances(X, inX)
+def calc_n_neighbor(inX, X, N):
+    distances = sklearn.metrics.pairwise.euclidean_distances(X, inX)
     sortedDist = distances.reshape((distances.shape[0],)).argsort()
-    return sortedDist
+    return sortedDist[:N]
 
 
 def plot_interactive_scatter(low_dim_embedding, labels, inx, q):
     from matplotlib.patches import Circle
-    fig = plt.figure(figsize=(6, 6))
+    fig = plt.figure()
     ax = fig.add_subplot(1, 1, 1)
 
     low_dim_embedding = np.concatenate([low_dim_embedding, inx])
@@ -93,7 +91,7 @@ def plot_interactive_scatter(low_dim_embedding, labels, inx, q):
 
     # mark query
     c_x, c_y = inx[0]
-    circle = Circle((c_x, c_y), 2, facecolor='none',
+    circle = Circle((c_x, c_y), 1, facecolor='none',
                     edgecolor='red', linewidth=3, alpha=0.5)
     ax.add_patch(circle)
 
