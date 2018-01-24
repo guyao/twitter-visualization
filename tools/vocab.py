40c56a400bdcb5493083ed23d33e343dee92f0a2
guyao <guyao@guyao.me>
2018-01-23 22:02:54 -0800

    add new tool: vocab

---
+++ tools/vocab.py
@@ -0,0 +1,52 @@
+from fastText import load_model
+import argparse
+import sys
+import numpy
+
+
+def output(line, filename):
+    with open(filename, "a") as f:
+        f.write(line)
+        f.write("\n")
+
+
+def words(model, filename):
+    for word in model.get_words():
+        output(word, filename)
+
+
+def vectors(model, filename):
+    for word in sys.stdin:
+        line = " ".join([str(i) for i in model.get_word_vector(word)])
+        output(line, filename)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=(
+            "Embedding word tool"
+        )
+    )
+
+    parser.add_argument(
+        "model",
+        help="Model to use",
+    )
+
+    parser.add_argument(
+        "command",
+        help="command=[words|vectors]",
+    )
+
+    parser.add_argument(
+        "output",
+        help="Output filename",
+    )
+
+    args = parser.parse_args()
+
+    model = load_model(args.model)
+    if args.command == "words":
+        words(model, args.output)
+    elif args.command == "vectors":
+        vectors(model, args.output)
